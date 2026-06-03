c74266e6b8e6f1ede0bcd5516283f6c492acf321
ggustavo-souza <cavalogames1231@gmail.com>
2026-06-03 15:38:32 -0300

    feat: implementando serviços de senha na API.

--- server/services/senhaServices.py
+++ server/services/senhaServices.py
@@ -1,1 +1,11 @@
 import bcrypt
+
+def encriptarSenha(senha):
+    hashSenha = bcrypt.hashpw(senha, bcrypt.gensalt())
+    return hashSenha
+
+def verificarSenha(senha, senhaBanco):
+    if(bcrypt.checkpw(senha, senhaBanco)):
+        return True
+    return False    
+
