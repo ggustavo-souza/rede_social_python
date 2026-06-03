ca6db27d36726ae4d2e2d83a740b11be7a1505db
ggustavo-souza <cavalogames1231@gmail.com>
2026-06-03 15:39:35 -0300

    feat: finalizando o controlador do login junto do de registrar

--- server/controllers/loginController.py
+++ server/controllers/loginController.py
@@ -1,8 +1,9 @@
+from sqlalchemy.sql.functions import session_user
 from models.user import UserModel
-from services.senhaServices import encriptarSenha
-from sqlalchemy.orm import sessionmaker, Session
+from services.senhaServices import encriptarSenha, verificarSenha
+from sqlalchemy.orm import Session
 from database import BD, User
-from middlewares.validarLogin import validarlogin
+from middlewares.validarLogin import validarlogin, validarRegistro
 
 bd = BD.getEngine()
 
@@ -11,11 +12,20 @@ class loginController(UserModel):
         if(validarlogin(request)):
             senhaEncriptada = encriptarSenha(request.senha)
             with Session(bd) as session:
-                query = session.query(User).filter(User.nome == request.nome).first()
-                if(query != None):
-                    query = session.query(User).filter(User.senha == senhaEncriptada).first()
-                    if(query != None):
-                        return {"message": "O login foi efetuado com sucesso!"}
+                usuarioBanco = session.query(User.nome).where(User.nome == request.nome)
+                if(usuarioBanco != None):
+                    senhaBanco = session.query(User.senha).where(User.nome == request.nome)
+                    if(verificarSenha(senhaEncriptada, senhaBanco)): 
+                        return {"message": "Login Efetuado com sucesso"}
+    def handleRegistro(request: UserModel):
+        if(validarRegistro(request)):
+            senhaEncriptada = encriptarSenha(request.senha)
+            with Session(bd) as session:
+                novoUsuario = User(nome=request.nome,email=request.email,senha=senhaEncriptada)
+                session.add(novoUsuario)
+                session.commit()
+                return {"message": "Conta criada com sucesso!"}
+
 
 
 
