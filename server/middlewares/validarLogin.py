97291817052bcd55cb1c69d5e88719edaf0c0c5e
ggustavo-souza <cavalogames1231@gmail.com>
2026-06-03 15:39:54 -0300

    feat: implementando middleware de registro

--- server/middlewares/validarLogin.py
+++ server/middlewares/validarLogin.py
@@ -2,3 +2,8 @@ def validarlogin(request):
     if ((request.nome != "" and len(request.nome) <= 20) and (request.senha != "")):
         return True
     return False
+
+def validarRegistro(request):
+    if ((request.nome != "" and len(request.nome) <= 20) and (request.senha != "") and (request.email != "")):
+        return True
+    return False
