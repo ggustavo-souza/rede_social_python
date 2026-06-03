c992a5699ccd4e72aed69f44c853adfcfb6e5454
ggustavo-souza <cavalogames1231@gmail.com>
2026-06-03 15:48:25 -0300

    feat: fazendo com que as rotas chamem os controllers

--- server/routes/loginRoutes.py
+++ server/routes/loginRoutes.py
@@ -1,9 +1,13 @@
+from models.user import UserModel
 from fastapi import FastAPI
+from controllers.loginController import loginController
 
 app = FastAPI()
 
-#@app.post("/login")
-#chamando Controller para lidar com o login 
+@app.post("/login")
+def postLogin(request: UserModel):
+    loginController.handleLogin(request)
 
-#app.post("/registrar")
-#chamando Controller para lidar com o registro de um novo usuário
+app.post("/registrar")
+def postRegistrar(request: UserModel):
+    loginController.handleRegistro(request)
