3eb3992ad3b7d5017aa270fdd6b9f42ba106c988
ggustavo-souza <cavalogames1231@gmail.com>
2026-06-08 14:33:31 -0300

    feat: realizando instalação do tailwind CSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router"
 import { BrowserRouter as Router } from "react-router"
+import Home from "./pages/Home"
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
     <>
       <Router>
         <Routes>
-
+          <Route path="/"><Home /></Route>
         </Routes>
       </Router>
     </>
