c1fe7df5710610d287558cfb008da7c513859c27
ggustavo-souza <cavalogames1231@gmail.com>
2026-06-12 16:17:41 -0300

    feat: limpando o arquivo home e criando a rota para a página de login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router"
 import { BrowserRouter as Router } from "react-router"
 import Home from "./pages/Home"
+import './assets/css/index.css'
+import LoginPage from "./pages/LoginPage"
 
 function App() {
 
@@ -9,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route index element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
     </>
