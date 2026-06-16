c38ce589cd7c9b9ac79282492523ef8bf195cf1b
ggustavo-souza <cavalogames1231@gmail.com>
2026-06-16 14:45:51 -0300

    feat(ui): criando o pedaço ao lado da tela para o usuário realizar login com o Google ou se registrar.

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,3 +1,4 @@
+import Botao from "../components/elements/BotaoSubmit"
 import LoginRegisterForm from "../components/ui/LoginRegisterForm"
 
 export default function LoginPage() {
@@ -5,12 +6,18 @@ export default function LoginPage() {
         <>
             <main className="flex flex-row border-3 min-h-screen">
                 <LoginRegisterForm type="Login" />
-                <aside className="bg-[var(--color-tertiary)] flex flex-col min-h-screen p-10">
+                <aside className="bg-[var(--color-tertiary)] flex flex-col min-h-screen p-10 py-15">
                     <header className="text-[var(--color-primary)] font-semibold text-3xl text-center my-5">
                         <h1>Ainda não possui conta?</h1>
                     </header>
-                    <main>
-
+                    <main className="justify-center flex flex-col my-20 p-2 gap-2">
+                        <Botao texto="Entrar com Google" type="button" tamanho="lg" />
+                        <div className="my-5 flex gap-5 text-white">
+                            <span className="border-t-2 border-white w-full self-center"></span>
+                            <span className="font-bold">OU</span>
+                            <span className="border-t-2 border-white w-full self-center"></span>
+                        </div>
+                        <Botao texto="Criar minha conta" destino="/registrar" type="button" tamanho="lg" />
                     </main>
                 </aside>
             </main>
