f4f9a8493acd11e8de4e4e5f047528d2040d5708
ggustavo-souza <cavalogames1231@gmail.com>
2026-06-17 21:10:24 -0300

    feat(server): implementando as políticas do CORS e um endpoint padrão pra testar funcionamento da API

--- server/server.py
+++ server/server.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 from database import BD
 from routes.loginRoutes import router as login_router
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+origins = ["http://localhost:3000", "http://localhost:5173"]
+
+# configuracoes de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  #permite as origens de requisição baseando-se no array acima         
+    allow_credentials=True,           
+    allow_methods=["*"],             
+    allow_headers=["*"],            
+)
 
 app.include_router(login_router)
+
+@app.get("/")
+def boasVindas():
+    return {"mensagem": "Você se conectou com a API"}
 
 BD.createBD()
