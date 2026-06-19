8cc1494cf38c7a1ebf4230dd7df42105798a7353
ggustavo-souza <cavalogames1231@gmail.com>
2026-06-18 21:27:48 -0300

    feat(frontend/pages): criação da página de registrar e registro dela no router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from "react-router"
 import Home from "./pages/Home"
 import './assets/css/index.css'
 import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/registrar" element={<RegisterPage />} />
         </Routes>
       </Router>
     </>
