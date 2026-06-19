363e2724e05ebac75f80a95ecce55c8211f66e8e
ggustavo-souza <cavalogames1231@gmail.com>
2026-06-18 21:29:05 -0300

    feat(frontend/component-LoginRegisterForm): complementando a lógica do componente para melhor reutilização

--- client/src/components/ui/LoginRegisterForm.tsx
+++ client/src/components/ui/LoginRegisterForm.tsx
@@ -1,7 +1,7 @@
 import { useState, type ChangeEvent } from "react";
 import Botao from "../elements/BotaoSubmit"
-import { type LoginFormData } from "../../types/FormTypes";
-import { loginCall } from "../../services/loginCall";
+import { type LoginFormData, type RegistrarFormData } from "../../types/FormTypes";
+import { loginCall, registerCall } from "../../services/formsCall";
 
 interface LoginRegisterFormProps {
     type: "Login" | "Registrar"
@@ -13,13 +13,18 @@ export default function LoginRegisterForm({ type }: LoginRegisterFormProps) {
         senha: ""
     })
 
-    const handleLogin = (event: React.SubmitEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const [registerData, setRegisterData] = useState<RegistrarFormData>({
+        email: "",
+        nome: "",
+        senha: ""
+    })
 
-        loginCall(loginData)
+    const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>, type: "login" | "registrar") => {
+        event.preventDefault();
+        type == "login" ? loginCall(loginData) : registerCall(registerData);
     }
 
-    const handleChangeLogin = (event: ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target
         setLoginData((prevData) => ({
             ...prevData,
@@ -34,14 +39,14 @@ export default function LoginRegisterForm({ type }: LoginRegisterFormProps) {
                     <header className="font-semibold text-3xl text-center my-10">
                         <h1>Entre com a sua conta!</h1>
                     </header>
-                    <form className="flex flex-col" onSubmit={handleLogin}>
+                    <form className="flex flex-col" onSubmit={(e) => handleSubmit(e, "login")}>
                         <div className="flex flex-col my-2">
                             <label htmlFor="email">E-mail</label>
-                            <input value={loginData.email} onChange={handleChangeLogin} className="ring-2 active:ring-3 focus:outline-[var(--color-tertiary)] focus: ring-gray-300 my-2 p-3 rounded-sm" name="email" id="email" type="text" placeholder="Example@email.com" />
+                            <input value={loginData.email} onChange={handleChange} className="ring-2 active:ring-3 focus:outline-[var(--color-tertiary)] focus: ring-gray-300 my-2 p-3 rounded-sm" name="email" id="email" type="text" placeholder="Example@email.com" />
                         </div>
                         <div className="flex flex-col my-2">
                             <label htmlFor="senha">Senha</label>
-                            <input value={loginData.senha} onChange={handleChangeLogin} className="ring-2 active:ring-3 focus:outline-[var(--color-tertiary)] ring-gray-300 my-2 p-3 rounded-sm" name="senha" id="senha" type="password" placeholder="••••••••••" />
+                            <input value={loginData.senha} onChange={handleChange} className="ring-2 active:ring-3 focus:outline-[var(--color-tertiary)] ring-gray-300 my-2 p-3 rounded-sm" name="senha" id="senha" type="password" placeholder="••••••••••" />
                         </div>
                         <div className="flex justify-center my-10">
                             <Botao texto="ENTRAR" type="submit" tamanho="lg" />
@@ -51,6 +56,7 @@ export default function LoginRegisterForm({ type }: LoginRegisterFormProps) {
             )}
             {type == "Registrar" && (
                 <section>
+                    {/*Para fazer: Seção de registrar conta */}
                     <header>
                         <h1>Crie sua conta em um instante!</h1>
                     </header>
