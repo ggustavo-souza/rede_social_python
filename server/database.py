f90a13cae80d6305cfb25c028578da691bd27af3
ggustavo-souza <cavalogames1231@gmail.com>
2026-06-30 16:54:49 -0300

    feat(database): criando a tabela de posts

--- server/database.py
+++ server/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, func
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 # classe da base declarativa (utilizada pra manipular tabelas)
@@ -13,6 +13,16 @@ class User(Base):
     nome = Column(String(20), nullable=False)
     email = Column(String(50), nullable=False)
     senha = Column(String(255), nullable=False)
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    titulo = Column(String(50), nullable=False)
+    conteudo = Column(String(255), nullable=False)
+    foto = Column(String(255), nullable=True)
+    usuario_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    data = Column(DateTime(timezone=True), server_default=func.now())
 
 class BD: 
     bd = create_engine("sqlite:///banco.db", echo=False)
