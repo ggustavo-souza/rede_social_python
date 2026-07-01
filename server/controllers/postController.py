109908d8a9b95efe86d20b26074b58249d33fa3e
ggustavo-souza <cavalogames1231@gmail.com>
2026-07-01 08:08:52 -0300

    fix(server/posts): ajustando as respostas para conterem "success" e detalhando melhor a rota de get posts para devolver erros claros

--- server/controllers/postController.py
+++ server/controllers/postController.py
@@ -1,9 +1,16 @@
 from models.post import PostModel
 from sqlalchemy.orm import Session
 from database import Post
+from fastapi import HTTPException
 
 def getAllPosts(db: Session):
-    return db.query(Post).all()
+    posts = db.query(Post).all()
+    if(posts != ""):
+        raise HTTPException(status_code=200, detail="Posts capturados com sucesso!")
+        return posts
+    else:
+        raise HTTPException(status_code=404, detail="Não foi possível capturar os posts")
+        return 
 
 def postPosts(db: Session, post: PostModel):
     newPost = Post(titulo=post.titulo, conteudo=post.conteudo, foto=post.foto, usuario_id=post.usuario_id)
@@ -12,7 +19,7 @@ def postPosts(db: Session, post: PostModel):
     db.add(newPost)
     db.commit()
     db.refresh(newPost)
-    return {"message": "Post criado com sucesso", "post": newPost}
+    return {"message": "Post criado com sucesso", "post": newPost, "success": True}
 
 def editPost(db: Session, post: PostModel, id: int):
     postToEdit = db.query(Post).filter(Post.id == id).first()
@@ -23,7 +30,7 @@ def editPost(db: Session, post: PostModel, id: int):
     postToEdit.foto = post.foto
     db.commit()
     db.refresh(postToEdit)
-    return {"message": "Post editado com sucesso", "post": postToEdit}
+    return {"message": "Post editado com sucesso", "post": postToEdit, "success": True}
 
 def deletePost(db: Session, id: int):
     postToDelete = db.query(Post).filter(Post.id == id).first()
@@ -31,4 +38,4 @@ def deletePost(db: Session, id: int):
         return {"message": "Post não encontrado"}
     db.delete(postToDelete)
     db.commit()
-    return {"message": "Post deletado com sucesso"}
+    return {"message": "Post deletado com sucesso", "success": True}
