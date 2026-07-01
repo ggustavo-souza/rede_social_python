5700dd4011b9e1f6edd01d39a43e832f92318701
ggustavo-souza <cavalogames1231@gmail.com>
2026-07-01 08:08:11 -0300

    feat(services/posts): função para dar fetch nos posts do banco de dados

--- client/src/services/postsCall.ts
+++ client/src/services/postsCall.ts
@@ -1,7 +1,28 @@
-async function getAllPosts() {
+const apiUrl = "http://localhost:8000"
 
+export async function getAllPosts() {
+    try {
+        const response = await fetch(`${apiUrl}/posts`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        if (response.ok) {
+            const data = await response.json()
+            return data
+        } else {
+            return false
+        }
+    } catch (e) {
+        if (e instanceof Error) {
+            console.error(e.message)
+            return false
+        }
+    }
 }
 
-async function createPost(formData: FormData) {
-    /*TODO: Fazer a requisição POST enviando os dados em FormData */
-}
+/* async function createPost(formData: FormData) {
+    TODO: Fazer a requisição POST enviando os dados em FormData 
+} */
