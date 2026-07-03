f56347aefc72e7ebf4b7741e16ee51979f6b7086
ggustavo-souza <cavalogames1231@gmail.com>
2026-07-03 17:12:24 -0300

    feat(client/frontend): criando a Home e sua lógica para capturar os posts do banco de dados e mostrá-los com o método .map percorrendo a lista capturada.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,48 @@
+import { useEffect, useState } from "react"
+import { getAllPosts } from "../services/postsCall"
+import Loading from "../components/elements/Loading";
+import { type Post } from "../types/PostType"
+
 export default function Home() {
+
+    const [loading, setLoading] = useState(true)
+    const [posts, setPosts] = useState<Post[]>([])
+    const imagesUrl: string = "http://localhost:8000/public/"
+
+    async function catchPosts() {
+        try {
+            const getPosts = await getAllPosts();
+            setPosts(getPosts)
+        } catch (e) {
+            if (e instanceof Error) {
+                console.error(e.message)
+            }
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        catchPosts();
+    }, [])
 
     return (
         <>
+            {loading && <Loading />}
 
+            {posts.length === 0 ? (
+                <>
+                    <p>Nenhum post foi encontrado.</p>
+                </>
+            ) : (
+                posts.map((post: Post) => (
+                    <div key={post.id}>
+                        <p>{post.titulo}</p>
+                        <p>{post.conteudo}</p>
+                        <p>{post.data}</p>
+                        <img src={`${imagesUrl}${post.foto}`} />
+                    </div>
+                )))}
         </>
     )
 }
---
+++ client/src/types/PostType.ts
@@ -0,0 +1,8 @@
+export interface Post {
+    id: number,
+    titulo: string,
+    conteudo: string,
+    foto: string,
+    usuario_id: number,
+    data: string
+}
