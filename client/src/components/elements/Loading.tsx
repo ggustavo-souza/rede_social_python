1e1f04eab5b7af6fa4633d6e538f3eaf69c0c640
ggustavo-souza <cavalogames1231@gmail.com>
2026-07-03 17:06:14 -0300

    feat(ui-ux/client): criando componente de loading

---
+++ client/src/components/elements/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <>
+            <div className="w-20 h-20 rounded-full animate-spin border-6 border-solid border-[var(--color-secondary)] border-t-transparent"> </div>
+        </>
+    )
+}
