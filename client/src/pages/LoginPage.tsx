7f0507608936c1386c06878db2e047c3fc1e96e5
ggustavo-souza <cavalogames1231@gmail.com>
2026-08-13 22:55:03 -0300

    fix: removendo botao de login com google

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -6,18 +6,12 @@ export default function LoginPage() {
         <>
             <main className="flex flex-row border-3 min-h-screen">
                 <LoginForm />
-                <aside className="bg-(--color-tertiary) flex flex-col min-h-screen p-10 py-5">
+                <aside className="bg-(--color-tertiary) flex flex-col min-h-screen p-10 py-20">
                     <img className="mx-auto" src="/image.png" height="70" width="70"></img>
                     <header className="text-(--color-primary) font-semibold text-3xl text-center my-5">
                         <h1>Ainda não possui conta?</h1>
                     </header>
-                    <main className="justify-center flex flex-col my-20 p-2 gap-2">
-                        <Botao texto="Entrar com Google" type="button" tamanho="lg" />
-                        <div className="my-5 flex gap-5 text-white">
-                            <span className="border-t-2 border-white w-full self-center"></span>
-                            <span className="font-bold">OU</span>
-                            <span className="border-t-2 border-white w-full self-center"></span>
-                        </div>
+                    <main className="justify-center flex flex-col p-2 gap-2">
                         <Botao texto="Criar minha conta" destino="/registrar" type="button" tamanho="lg" />
                     </main>
                 </aside>
